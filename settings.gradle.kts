2b43d7a4eb381ee9bb215f69f10681c2038bcb08
user.email <younsoo6103@naver.com>
2025-05-29 15:44:20 +0900

    step 1, r

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "P14017-2"
---
+++ src/test/java/com/back/RqTest.java
@@ -0,0 +1,17 @@
+package com.back;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RqTest {
+    @Test
+    @DisplayName("rq.getActionName()")
+    void t1(){
+        Rq rq = new Rq("수정?id=1");
+
+        String actionName = rq.getActionName();
+
+        assertEquals("수정", actionName);
+    }
+}
